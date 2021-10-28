4c5cef845888fd868f599a84dfac3c10af3711b4
Spencer Peterson <u0927156@utah.edu>
2021-10-27 19:25:07 -0600

    Added filtering functionality
    Requested type of line in

--- Problem1.py
+++ Problem1.py
@@ -7,12 +7,35 @@ Created on Wed Oct 27 18:40:54 2021
 import sys,os
 import requests
 
-print(os.path.dirname(__file__))
-sys.path.append(os.path.dirname(__file__))
 
-
+# read in my API key. Keep it secret, keep it safe.
 f = open('key.txt', 'r')
 key = f.read()
 f.close()
 
-r = requests.get("https://api-v3.mbta.com/lines")
+
+# Request all of the line information and include . 
+r = requests.get("https://api-v3.mbta.com/routes?fields[type]", headers={'x-api-key': key})
+
+# I'm going to filter information locally. I chose to do this because I want to
+# keep my requests to a minimum and it shouldn't be computationally intensive
+# on my end.
+
+# %% 
+
+if r.status_code != 200:
+    print('Something went wrong :/')
+else:
+    print('Filtering routes')
+    
+    
+data = r.json()['data']
+
+subway = []
+
+for datum in data:
+    
+    if datum['attributes']['type'] == 0 or datum['attributes']['type'] == 1:
+        subway.append(datum)
+        
+        
