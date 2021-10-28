703c85a41efdb2a8392d9190e11b1cd3557c5354
Spencer Peterson <u0927156@utah.edu>
2021-10-27 19:54:01 -0600

    Added stops request
    Added request to get stops and added dummy code for printing the answer to the first two questions of question 2.

--- MBTAStopProgram.py
+++ MBTAStopProgram.py
@@ -8,13 +8,22 @@ Created on Wed Oct 27 18:40:54 2021
 import requests
 
 
-def main():
-    # read in my API key. Keep it secret, keep it safe.
-    f = open('key.txt', 'r')
-    key = f.read()
-    f.close()
-    
-    
+
+def getSubwayLines(key):
+    """
+    Get all active subway lines
+
+    Parameters
+    ----------
+    key : string
+        The api key.
+
+    Returns
+    -------
+    List of dicts
+        List of dictionaries containing the information of subway lines.
+
+    """
     # Request all of the line information and include . 
     r = requests.get("https://api-v3.mbta.com/routes?fields[type]", headers={'x-api-key': key})
     
@@ -24,9 +33,7 @@ def main():
 
     
     if r.status_code != 200:
-        print('Something went wrong with the request.')
-    else:
-        print('Filtering routes')
+        return []
         
         
     data = r.json()['data']
@@ -37,7 +44,26 @@ def main():
         
         if datum['attributes']['type'] == 0 or datum['attributes']['type'] == 1:
             subway_lines.append(datum)
-            
+    
+    return subway_lines
+    
+def getStops(lineID, key):
+    
+    
+    r = requests.get(f"https://api-v3.mbta.com/stops?filter%5Broute%5D={lineID}", headers={'x-api-key': key})
+    
+    stops = r.json()['data']
+
+    return stops    
+def main():
+    # read in my API key. Keep it secret, keep it safe.
+    f = open('key.txt', 'r')
+    key = f.read()
+    f.close()
+    
+    
+   
+    subway_lines = getSubwayLines(key)
             
     for line in subway_lines:
         print(line['attributes']['long_name'])
