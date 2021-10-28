ec807791e33139b9eca4e5b744d6dcbbe5f7bd75
Spencer Peterson <u0927156@utah.edu>
2021-10-27 19:32:44 -0600

    Finished Problem 1
    Made problem1 solution into a proper python script that can be run from the command line.
    
    Made problem2 file.

--- Problem1.py
+++ Problem1.py
@@ -4,40 +4,45 @@ Created on Wed Oct 27 18:40:54 2021
 
 @author: Spencer Peterson
 """
-import sys,os
+
 import requests
 
 
-# read in my API key. Keep it secret, keep it safe.
-f = open('key.txt', 'r')
-key = f.read()
-f.close()
-
-
-# Request all of the line information and include . 
-r = requests.get("https://api-v3.mbta.com/routes?fields[type]", headers={'x-api-key': key})
-
-# I'm going to filter information locally. I chose to do this because I want to
-# keep my requests to a minimum and it shouldn't be computationally intensive
-# on my end.
-
-# %% 
-
-if r.status_code != 200:
-    print('Something went wrong :/')
-else:
-    print('Filtering routes')
+def main():
+    # read in my API key. Keep it secret, keep it safe.
+    f = open('key.txt', 'r')
+    key = f.read()
+    f.close()
     
     
-data = r.json()['data']
-
-subway_lines = []
-
-for datum in data:
+    # Request all of the line information and include . 
+    r = requests.get("https://api-v3.mbta.com/routes?fields[type]", headers={'x-api-key': key})
     
-    if datum['attributes']['type'] == 0 or datum['attributes']['type'] == 1:
-        subway_lines.append(datum)
+    # I'm going to filter information locally. I chose to do this because I want to
+    # keep my requests to a minimum and it shouldn't be computationally intensive
+    # on my end.
+
+    
+    if r.status_code != 200:
+        print('Something went wrong with the request.')
+    else:
+        print('Filtering routes')
         
         
-for line in subway_lines:
-    print(line['attributes']['long_name'])
+    data = r.json()['data']
+    
+    subway_lines = []
+    
+    for datum in data:
+        
+        if datum['attributes']['type'] == 0 or datum['attributes']['type'] == 1:
+            subway_lines.append(datum)
+            
+            
+    for line in subway_lines:
+        print(line['attributes']['long_name'])
+        
+
+
+if __name__ == "__main__":
+    main()
